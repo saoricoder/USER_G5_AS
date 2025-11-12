0694cbf239337a8ce87acfd3cd687264311a3aba
saoricoder <blrodriguez3@espe.edu.ec>
2025-11-11 22:49:15 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,35 +2,11 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-=======
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
->>>>>>> 14cf3bcac3ebac2fb5529fd0688b807209df5057
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-<<<<<<< HEAD
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
-
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'fecha_nacimiento',
-        'sexo',
-        'numero_seguro',
-        'historial_medico', 
-        'contacto_emergencia',
-    ];
-
-=======
 
 
 class User extends Authenticatable
@@ -59,37 +35,11 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
->>>>>>> 14cf3bcac3ebac2fb5529fd0688b807209df5057
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-<<<<<<< HEAD
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-        'fecha_nacimiento' => 'date',
-    ];
-
-    // RELACIONES
-    public function doctor(): HasOne
-    {
-        return $this->hasOne(Doctor::class, 'user_id');
-    }
-
-    public function citas(): HasMany
-    {
-        return $this->hasMany(Cita::class, 'paciente_id');
-    }
-
-    public function historiasMedicas(): HasMany
-    {
-        return $this->hasMany(HistoriaMedica::class, 'paciente_id');
-    }
-}
-// Fin de app/Models/User.php
-=======
     /**
      * Get the attributes that should be cast.
      *
@@ -103,4 +53,3 @@ class User extends Authenticatable
         ];
     }
 }
->>>>>>> 14cf3bcac3ebac2fb5529fd0688b807209df5057
